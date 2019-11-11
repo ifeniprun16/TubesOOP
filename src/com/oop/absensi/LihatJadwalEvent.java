29aea30c66b32a3bc9489c646460d019ee745a8a
ASUS <ASUS@10.20.16.131>
2019-11-11 14:33:01 +0700

    nambah gui

--- src/com/oop/absensi/LihatJadwalEvent.java
+++ src/com/oop/absensi/LihatJadwalEvent.java
@@ -69,7 +69,7 @@ public class LihatJadwalEvent extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Event", "Tanggal", "Waktu", "Tempat"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
